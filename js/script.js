e90f25a5f441ec764a5ec4ab4f061de47bde259a
frponti <30413782+frponti@users.noreply.github.com>
2026-04-13 16:52:09 -0400

    Update script.js
    url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const RADIO_NAME = 'Jailson Web Rádio';
+const RADIO_NAME = 'ROCK';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stream.zeno.fm/yn65fsaurfhvv';
+const URL_STREAMING = 'https://radios.panchoromero.cl/rock';
 
 //API URL /
 const API_URL = 'https://twj.es/free/?url='+URL_STREAMING;
